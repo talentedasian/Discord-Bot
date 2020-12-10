11e10be64b11c8f6cd49a03531a69e4552f87790
Almir James <lucenaalmirjames123@gmail.com>
2020-12-10 11:31:43 +0800

    refactored qulaify role to get names iwth spaces

--- src/main/java/GuildRoles/MemberQualifyForRole.java
+++ src/main/java/GuildRoles/MemberQualifyForRole.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -17,9 +18,11 @@ public class MemberQualifyForRole extends ListenerAdapter {
         TextChannel channelName = event.getGuild().getTextChannelsByName("role-room", true).get(0);
         if ("!role".equals(message[0]) && "request".equals(message[1]) && channelName.equals(event.getChannel())) {
             String roleId = event.getGuild().getRolesByName(message[2], true).get(0).getId();
-            event.getChannel().sendMessage("<@" + roleId + "> Check if he meets the criteria").queue();
+            event.getChannel().sendMessage("<@Moderator> Check if he meets the criteria").queue();
         } else if ("!role".equals(message[0]) && "add".equals(message[1]) && channelName.equals(event.getChannel())) {
-            event.getGuild().addRoleToMember(event.getGuild().getMembersByName(message[2],true).get(0), event.getGuild().getRolesByName(message[3],true).get(0)).queue();
+            event.getGuild().addRoleToMember(event.getGuild().getMembersByName(Arrays.stream(message)
+                    .findFirst().get(),true).get(0)
+                    , event.getGuild().getRolesByName(message[-1],true).get(0)).queue();
             event.getChannel().sendMessage("Role Added").queue();
         } else if (message[0].equalsIgnoreCase("!role")  && !channelName.equals(event.getChannel())) {
                 event.getChannel().sendMessage("Please Go To The Correct Room For this Message")
