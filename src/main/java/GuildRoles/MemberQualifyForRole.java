d61f399bb7e9d70491fb395d99d1c2470aba6df5
Almir James <lucenaalmirjames123@gmail.com>
2020-12-10 12:01:44 +0800

    refactored qualify role. added more secured permissions fors ettign role

--- src/main/java/GuildRoles/MemberQualifyForRole.java
+++ src/main/java/GuildRoles/MemberQualifyForRole.java
@@ -5,8 +5,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class MemberQualifyForRole extends ListenerAdapter {
@@ -16,13 +15,13 @@ public class MemberQualifyForRole extends ListenerAdapter {
         super.onGuildMessageReceived(event);
         String[] message = event.getMessage().getContentRaw().split(" ");
         TextChannel channelName = event.getGuild().getTextChannelsByName("role-room", true).get(0);
+        String roleId = event.getGuild().getRolesByName(message[-1], true).get(0).getId();
+        Role role = event.getGuild().getRolesByName("Moderator", true).get(0);
         if ("!role".equals(message[0]) && "request".equals(message[1]) && channelName.equals(event.getChannel())) {
-            String roleId = event.getGuild().getRolesByName(message[2], true).get(0).getId();
             event.getChannel().sendMessage("<@Moderator> Check if he meets the criteria").queue();
-        } else if ("!role".equals(message[0]) && "add".equals(message[1]) && channelName.equals(event.getChannel())) {
-            event.getGuild().addRoleToMember(event.getGuild().getMembersByName(Arrays.stream(message)
-                    .findFirst().get(),true).get(0)
-                    , event.getGuild().getRolesByName(message[-1],true).get(0)).queue();
+            } else if ("!role".equals(message[0]) && "add".equals(message[1]) && channelName.equals(event.getChannel())
+                && (Objects.requireNonNull(event.getMember()).getRoles().contains(role) || event.getMember().isOwner())) {
+            event.getGuild().addRoleToMember(event.getChannel().getAsMention(),event.getGuild().getRoleById(roleId)).queue();
             event.getChannel().sendMessage("Role Added You Can Now Enjoy The Benefits of Being a " + message[-1].toUpperCase()).queue();
         } else if (message[0].equalsIgnoreCase("!role")  && !channelName.equals(event.getChannel())) {
                 event.getChannel().sendMessage("Please Go To The Correct Room For this Message")
