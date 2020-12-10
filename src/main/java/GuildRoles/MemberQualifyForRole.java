4647fb99dcb98bcfbf74c9bef35db77c6f8a05f4
Almir James <lucenaalmirjames123@gmail.com>
2020-12-10 11:13:26 +0800

    refactored qualify for role

--- src/main/java/GuildRoles/MemberQualifyForRole.java
+++ src/main/java/GuildRoles/MemberQualifyForRole.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MemberQualifyForRole extends ListenerAdapter {
 
@@ -14,13 +15,17 @@ public class MemberQualifyForRole extends ListenerAdapter {
         super.onGuildMessageReceived(event);
         String[] message = event.getMessage().getContentRaw().split(" ");
         TextChannel channelName = event.getGuild().getTextChannelsByName("role-room", true).get(0);
-        Member user = event.getMember();
         if ("!role".equals(message[0]) && "request".equals(message[1]) && channelName.equals(event.getChannel())) {
-            String roleId = event.getGuild().getRolesByName(message[3], true).get(0).getId();
-            event.getChannel().sendMessage("@" + roleId + " Check if he meets the criteria").queue();
-        } else if ("!add".equals(message[0]) && "role".equals(message[1]) && channelName.equals(event.getMessage().getTextChannel())) {
+            String roleId = event.getGuild().getRolesByName(message[2], true).get(0).getId();
+            event.getChannel().sendMessage("<@" + roleId + "> Check if he meets the criteria").queue();
+        } else if ("!add".equals(message[0]) && "role".equals(message[1]) && channelName.equals(event.getChannel())) {
             event.getGuild().addRoleToMember(event.getGuild().getMembersByName(message[2],true).get(0), event.getGuild().getRolesByName(message[3],true).get(0)).queue();
             event.getChannel().sendMessage("Role Added").queue();
+        } else if (!channelName.equals(event.getChannel())) {
+                event.getChannel().sendMessage("Please Go To The Correct Room For this Message")
+                        .queue(m -> m.delete().queueAfter(10, TimeUnit.SECONDS));
+                event.getMessage().delete().queueAfter(3, TimeUnit.SECONDS);
+
         }
 
     }
