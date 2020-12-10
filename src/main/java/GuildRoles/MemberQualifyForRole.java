b17a1ab703e3efb7276f8aee8e23609e90d40569
Almir James <lucenaalmirjames123@gmail.com>
2020-12-10 12:34:16 +0800

    fixed indexout of bounds

--- src/main/java/GuildRoles/MemberQualifyForRole.java
+++ src/main/java/GuildRoles/MemberQualifyForRole.java
@@ -18,10 +18,10 @@ public class MemberQualifyForRole extends ListenerAdapter {
 
         var channelName = event.getGuild().getTextChannelsByName("role-room", true).get(0);
         if ("!role".equals(message[0]) && "request".equals(message[1]) && event.getChannel().equals(channelName)) {
-            event.getChannel().sendMessage("<@Moderator> Check if he meets the criteria").queue();
+            event.getChannel().sendMessage("<@ " + role.getId() + "> Check if he meets the criteria").queue();
             } else if ("!role".equals(message[0]) && "add".equals(message[1]) && event.getChannel().equals(channelName)
                 && (Objects.requireNonNull(event.getMember()).getRoles().contains(role) || event.getMember().isOwner())) {
-            event.getGuild().addRoleToMember(event.getChannel().getAsMention(),event.getGuild().getRoleById(message[-1])).queue();
+            event.getGuild().addRoleToMember(event.getChannel().getAsMention(),event.getGuild().getRoleById(message[3])).queue();
             event.getChannel().sendMessage("Role Added You Can Now Enjoy The Benefits of Being a " + message[2].toUpperCase()).queue();
         } else if (message[0].equalsIgnoreCase("!role")  && !event.getChannel().equals(channelName)) {
                 event.getChannel().sendMessage("Please Go To The Correct Room For this Message")
