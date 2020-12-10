484c3e6928204fdfa8b96838cae48f50d1322168
Almir James <lucenaalmirjames123@gmail.com>
2020-12-10 12:15:32 +0800

    qualify for role not working fixed(not yet tested)

--- src/main/java/GuildRoles/MemberQualifyForRole.java
+++ src/main/java/GuildRoles/MemberQualifyForRole.java
@@ -14,16 +14,16 @@ public class MemberQualifyForRole extends ListenerAdapter {
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
         super.onGuildMessageReceived(event);
         String[] message = event.getMessage().getContentRaw().split(" ");
-        TextChannel channelName = event.getGuild().getTextChannelsByName("role-room", true).get(0);
-        String roleId = event.getGuild().getRolesByName(message[-1], true).get(0).getId();
+        String roleId = event.getGuild().getRolesByName(message[3], true).get(0).getId();
         Role role = event.getGuild().getRolesByName("Moderator", true).get(0);
-        if ("!role".equals(message[0]) && "request".equals(message[1]) && channelName.equals(event.getChannel())) {
-            event.getChannel().sendMessage("<@Moderator> Check if he meets the criteria").queue();
-            } else if ("!role".equals(message[0]) && "add".equals(message[1]) && channelName.equals(event.getChannel())
+        var channelName = event.getChannel().toString();
+        if ("!role".equals(message[0]) && "request".equals(message[1]) && event.getChannel().toString().equalsIgnoreCase("role-room")) {
+            event.getChannel().sendMessage("<@" + role + "> Check if he meets the criteria").queue();
+            } else if ("!role".equals(message[0]) && "add".equals(message[1]) && channelName.equalsIgnoreCase("role-room")
                 && (Objects.requireNonNull(event.getMember()).getRoles().contains(role) || event.getMember().isOwner())) {
             event.getGuild().addRoleToMember(event.getChannel().getAsMention(),event.getGuild().getRoleById(roleId)).queue();
-            event.getChannel().sendMessage("Role Added You Can Now Enjoy The Benefits of Being a " + message[-1].toUpperCase()).queue();
-        } else if (message[0].equalsIgnoreCase("!role")  && !channelName.equals(event.getChannel())) {
+            event.getChannel().sendMessage("Role Added You Can Now Enjoy The Benefits of Being a " + message[3].toUpperCase()).queue();
+        } else if (message[0].equalsIgnoreCase("!role")  && !channelName.equalsIgnoreCase("role-room")) {
                 event.getChannel().sendMessage("Please Go To The Correct Room For this Message")
                         .queue(m -> m.delete().queueAfter(10, TimeUnit.SECONDS));
                 event.getMessage().delete().queueAfter(3, TimeUnit.SECONDS);
