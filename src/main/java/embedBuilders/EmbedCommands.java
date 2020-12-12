e56c4e55a3dba708aef94d5550962a25e6a92dc4
Almir James <lucenaalmirjames123@gmail.com>
2020-12-12 14:07:51 +0800

    removed redundant code

--- src/main/java/embedBuilders/EmbedCommands.java
+++ src/main/java/embedBuilders/EmbedCommands.java
@@ -1,15 +1,8 @@
 package embedBuilders;
 
 import java.awt.Color;
-import java.util.List;
-
-import botCommands.Miscellaneous.*;
-import main.MainMethod;
-import net.aksingh.owmjapis.api.APIException;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.TextChannel;
 
 public class EmbedCommands {
 
@@ -26,54 +19,4 @@ public class EmbedCommands {
 
 		}
 
-		public MessageEmbed textsDouble(Double value,Color color){
-			embed.clearFields();
-			return new EmbedBuilder(embed).setColor(color).build();
-		}
-
-		public String textString(String str){
-			embed.clearFields();
-			return str;
-		}
-		public GifCommand gifTextMessage(){
-			embed.clearFields();
-			return new GifCommand();
-		}
-
-		public String getWeather(String city) throws APIException {
-			embed.clearFields();
-			return new WeatherCommand().getWeather(city);
-		}
-
-		public String rollDice(){
-			embed.clearFields();
-			return new RollCommand().run();
-		}
-
-		public String eightBall(){
-			embed.clearFields();
-			return new EightballCommand().run();
-		}
-
-		public String time(){
-			embed.clearFields();
-			return new TimeCommand().run();
-		}
-
-		public static EmbedBuilder noFieldMessageEmbed(String name, List<String> msgs, Color color, MessageChannel channel) {
-			EmbedBuilder builder = new EmbedBuilder();
-			builder.setColor(color);
-			if(msgs.size() != 0) {
-				String finalMsg = msgs.get(0);
-				for (int i = 1; i < msgs.size(); i++) {
-					finalMsg = finalMsg + "\n" + msgs.get(i);
-				}
-				builder.addField(name, finalMsg, false);
-			}
-			if (channel instanceof TextChannel) {
-				builder.setFooter(((TextChannel) channel).getGuild().getName() + " - " + MainMethod.class.getName(),
-						((TextChannel) channel).getGuild().getIconUrl());
-			}
-			return builder;
-		}
 }
