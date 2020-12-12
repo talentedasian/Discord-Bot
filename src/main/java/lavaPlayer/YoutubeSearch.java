6b8232d826630985cb750f82728e0e767f1e6dde
Almir James <lucenaalmirjames123@gmail.com>
2020-12-12 19:17:16 +0800

    fixed mass off

--- src/main/java/lavaPlayer/YoutubeSearch.java
+++ src/main/java/lavaPlayer/YoutubeSearch.java
@@ -98,7 +98,7 @@ public class YoutubeSearch extends ListenerAdapter {
             event.getChannel().sendMessage("**MAY MISA TAYO!!!!!** " + event.getGuild().getRoleById(mention).getAsMention()).queue();
         } else if (!event.getAuthor().isBot() && supposedChannel.equals(event.getGuild().getTextChannelById(channelId))
                 && musicManagers.get(event.getGuild().getIdLong()).scheduler.isMass() && event.getMember().getRoles().contains(event.getGuild().getRoleById(moderator))
-                && !event.getMessage().equals("!massoff")) {
+                && !command[0].equals("!massoff") && !command.equals("~set")) {
             event.getMessage().delete().queueAfter(4, TimeUnit.SECONDS);
             event.getChannel().sendMessage("Bawal Maingay sa **MISA TANG INA**").queue();
         } else if ("!massoff".equals(command[0]) && event.getMember().getRoles().contains(event.getGuild().getRoleById(moderator))) {
